0799794dc4ebc996284481e0d71cf607c9be67dc
devsvc <open.dev@outlook.com>
2021-07-20 02:32:10 +0800

    notion connector

---
+++ zentao-unitask-connector/src/main/kotlin/pro/devsvc/unitask/connector/zentao/ZentaoConnector.kt
@@ -0,0 +1,11 @@
+package pro.devsvc.unitask.connector.zentao
+
+import pro.devsvc.unitask.connector.Connector
+import pro.devsvc.unitask.store.nitrite.TaskStore
+
+class ZentaoConnector() : Connector {
+
+    override fun start(store: TaskStore) {
+
+    }
+}
